41cb4255a2127d91a09839dbd1105804f1d4b310
philip.basnet <philip.basnet@ensue.us>
2022-05-01 15:25:27 +0545

    added packages and basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css,scss,sass}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      dank: ["Dank Mono", "Cascadia Mono", "monospace"],
+    },
+  },
+  plugins: [],
+};
